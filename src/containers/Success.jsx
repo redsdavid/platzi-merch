d893f33e92be6858c57ade4472c7be745bb5ae64
redsdavid <redsdavid93@gmail.com>
2021-12-15 18:48:53 -0500

    Add project Files

---
+++ src/containers/Success.jsx
@@ -0,0 +1,27 @@
+import React, {useContext} from 'react'
+import AppContext from '../context/AppContext'
+import Map from '../components/Map'
+import usePositionStackAddress from '../hooks/usePositionStackAddress'
+import '../styles/components/Success.css'
+
+const Success = () => {
+    const {state} = useContext(AppContext);
+    const { buyer } = state;
+    console.log("address is " + buyer[0].address);
+    const location = usePositionStackAddress(buyer[0].address);
+    console.log(location);
+
+    return (
+        <div className="Success">
+            <div className="Success-content">
+                <h2>{`${buyer[0].name}!!!  .... Gracias por tu compra !! :)` }</h2>
+                <span>Tu pedido llegará en 3 dias a tu dirección</span>
+                <div className="success-map">
+                    <Map data={location}/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Success
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new Dotenv(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css',
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: true,
+    port: 3005,
+  },
+};
