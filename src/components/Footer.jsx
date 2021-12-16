da224f7aecddfeebe585cf84ddf691eeb112da4b
redsdavid <redsdavid93@gmail.com>
2021-12-15 20:39:21 -0500

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/components/Footer.css';
+
+function Footer() {
+    return (
+        <div>
+            <div className="Footer">
+                <p className="Footer-title">Platzi Conf Merch</p>
+                <p className="Footer-copy">Todos los rights reservados ¡NEA!</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
